3342f42d0b542db7edc4209fd1f8cf4abfd93e6d
Luigi T <luigitejada@yahoo.com>
2023-03-21 13:44:12 -0400

    add url, view, and form for adding new topics

--- learning_diary/forms.py
+++ learning_diary/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
-from .models import Topic
+from .models import Topic, Entry
 
 class TopicForm(forms.ModelForm):
   class Meta:
     model = Topic
     fields = ['text']
     labels = {'text':''}
-    
+
--- learning_diary/views.py
+++ learning_diary/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 def index(request):
@@ -18,4 +19,20 @@ def topic(request, topic_id):
   entries = topic.entry_set.order_by('-date_added')
   context = {'topic': topic, 'entries':entries}
   return render(request, 'learning_diary/topic.html', context)
+
+def new_topic(request):
+  '''Add a new topic.'''
+  if request.method != 'POST':
+    # No data submitted; create a blank form.
+    form = TopicForm()
+  else:
+    # POST data submitted; process data.
+    form = TopicForm(data=request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('learning_diary:topics')
+  
+  # Display a blank or invalid form.
+  context = {'form': form}
+  return render(request, 'learning_diary/new_topic.html', context)
   
