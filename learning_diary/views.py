2e26fbf3b857e60124d37e25f7060992b26447c2
Luigi T <luigitejada@yahoo.com>
2023-03-22 10:43:28 -0400

    impliment information protection so user can only see their own content

--- learning_diary/views.py
+++ learning_diary/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -11,17 +13,24 @@ def index(request):
 @login_required
 def topics(request):
   '''Show all topics.'''
-  topics = Topic.objects.order_by('date_added')
+  topics = Topic.objects.filter(owner = request.user).order_by('date_added')
+  
   context = {'topics': topics}
   return render(request, 'learning_diary/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
   '''Show a single topic and all its entries.'''
   topic = Topic.objects.get(id=topic_id)
+  # Make sure the topic belongs to the current user.
+  if topic.owner != request.user:
+    raise Http404
+  
   entries = topic.entry_set.order_by('-date_added')
   context = {'topic': topic, 'entries':entries}
   return render(request, 'learning_diary/topic.html', context)
 
+@login_required
 def new_topic(request):
   '''Add a new topic.'''
   if request.method != 'POST':
@@ -31,13 +40,16 @@ def new_topic(request):
     # POST data submitted; process data.
     form = TopicForm(data=request.POST)
     if form.is_valid():
-      form.save()
+      new_topic = form.save(commit=False)
+      new_topic.owner = request.user
+      new_topic.save()
       return redirect('learning_diary:topics')
   
   # Display a blank or invalid form.
   context = {'form': form}
   return render(request, 'learning_diary/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
   '''Add a new entry for a particular topic.'''
   topic = Topic.objects.get(id=topic_id)
@@ -58,10 +70,14 @@ def new_entry(request, topic_id):
   context = {'topic':topic, 'form':form}
   return render(request, 'learning_diary/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
   '''Edit an existing entry.'''
   entry = Entry.objects.get(id=entry_id)
   topic = entry.topic
+  # Make sure the topic belongs to the current user.
+  if topic.owner != request.user:
+    raise Http404
 
   if request.method != 'POST':
     # Initial request; pre-fill form with the current entry.
